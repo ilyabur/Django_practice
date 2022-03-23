cd6729a2fa9904e03b2a96141839c8648208dd39
frontik <ilyaps3@mail.ru>
2022-03-23 21:24:10 +0300

    Adding articles for news with admin

--- second_app/models.py
+++ second_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Articles(models.Model): # создаем таблицу базы данных
+    title = models.CharField("Заголовок", max_length=50) # заголовок
+    anons = models.CharField("Анонс", max_length=250) # анонс статьи
+    full_text = models.TextField("Статья") # статья
+    date = models.DateTimeField("Дата публикации") # дата и время
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+
--- second_app/views.py
+++ second_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Articles
 
-# Create your views here.
+def news_home(request):
+    news = Articles.objects.all() # вместо all можно написть order_by(тайтл или название новости или новость) таким образом мы можем их сортировать
+    return render(request, 'second_app/news_home.html', {"news": news})
+
